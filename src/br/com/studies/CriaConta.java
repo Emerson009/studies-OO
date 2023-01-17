8ec05fb73e4d6a86c6e20c67bfe8fd23fad7ed15
Emerson009 <henriqueemerson023@gmail.com>
2023-01-17 10:57:49 -0300

    adicionando instancia

--- src/br/com/studies/CriaConta.java
+++ src/br/com/studies/CriaConta.java
@@ -1,5 +1,19 @@
 package br.com.studies;
 
 public class CriaConta {
-
+	
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saldo += 200;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("A primeira conta tem: "+primeiraConta.saldo);
+		System.out.println("A segunda conta tem: "+segundaConta.saldo);
+	}
 }
