7d6c80600fe4af68338c8e3131b881eb47393f43
Emerson009 <henriqueemerson023@gmail.com>
2023-01-18 12:55:55 -0300

    add-transferencia

--- src/br/com/studies/Conta.java
+++ src/br/com/studies/Conta.java
@@ -1,5 +1,7 @@
 package br.com.studies;
 
+import java.util.Scanner;
+
 public class Conta {
 	
 	double saldo;
@@ -7,8 +9,30 @@ public class Conta {
 	int numero;
 	String titular;
 	
+	Scanner scanner = new Scanner(System.in);
+	
 	public void depositar(double valor){
 		this.saldo = this.saldo + valor;
 	}
+	
+    public boolean saca(double valor) { 
+        if(this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else { 
+            return false;
+        }
+    }
+    
+    public boolean transferir(double valor, Conta destino) {
+    	if(this.saldo >= valor) {
+    		this.saldo -=  valor;
+    		destino.depositar(valor);
+    		return true;
+    	}else {
+    		return false;
+    	}
+    	
+    }
 
 }
--- src/br/com/studies/TestaMetodos.java
+++ src/br/com/studies/TestaMetodos.java
@@ -4,10 +4,25 @@ public class TestaMetodos {
 
 	public static void main(String[] args) {
 		Conta conta = new Conta();
-		conta.saldo = 100;
+		conta.saldo = 7;
 		
-		conta.depositar(500000);
+		conta.depositar(7);
 		System.out.println(conta.saldo);
 		
+        boolean conseguiuRetirar = conta.saca(7);
+        System.out.println(conta.saldo);
+        System.out.println(conseguiuRetirar);
+		
+        Conta conta2 = new Conta();
+        
+        conta2.depositar(1000);
+        
+        if(conta2.transferir(3000, conta)) {
+        	System.out.println("Tranferencia realizada com sucesso");
+        }else {
+        	System.out.println("faltou dinheiro");
+        }
+        System.out.println(conta2.saldo);
+        System.out.println(conta.saldo);
 	}
 }
